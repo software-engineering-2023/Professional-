29e092de6b123ec66f468f37fd814555943d649a
M-Bahy <Homelander.SolderBoy@gmail.com>
2023-05-29 22:39:43 +0200

    report technical issues

--- js/transfer.js
+++ js/transfer.js
@@ -11,6 +11,7 @@ document.addEventListener("DOMContentLoaded", function() {
   const anotherVitaCardForm = document.getElementById("anotherVitaCardForm");
   const transferOutsideVitaForm = document.getElementById("transferOutsideVitaForm");
   const ShowDetailsForm = document.getElementById("ShowDetailsForm");
+  const transferButton = document.getElementById("transferButton");
   
 
   // Event listener for transfer type selection
@@ -61,8 +62,66 @@ document.addEventListener("DOMContentLoaded", function() {
   transferForm.addEventListener("submit", function(event) {
     event.preventDefault();
     // Perform the transfer or any necessary actions here
-  });
+    // if (validateForm()) {
+    //   // Perform the transfer or any necessary actions here
+    //   console.log("Form submitted successfully");
+    // }
 
+    if (validateForm()) {
+      redirectToTransactionStatus();
+    }
+  
+  });
+  function validateForm() {
+    const formFields = transferForm.elements;
+    let isValid = true;
+  
+    for (let i = 0; i < formFields.length; i++) {
+      const field = formFields[i];
+      if (field.required) {
+        if (field.value.trim() === "" || (field.type === 'number' && isNaN(field.value))) {
+          isValid = false;
+          field.classList.add("error"); // Add a CSS class to highlight the field as an error
+        } else {
+          field.classList.remove("error"); // Remove the error class if the field is filled
+        }
+      }
+    }
+  
+    return isValid;
+  }
+  
+  // function validateForm() {
+  //   const amountInputs = document.querySelectorAll("input[name='amount']");
+  //   let isValid = true;
+  
+  //   // Check if each amount field contains only numbers
+  //   amountInputs.forEach(function(input) {
+  //     if (!/^\d+$/.test(input.value)) {
+  //       isValid = false;
+  //       input.classList.add("error");
+  //     } else {
+  //       input.classList.remove("error");
+  //     }
+  //   });
+  
+  //   return isValid;
+  // }
+  // transferButton.addEventListener("click", function(event){
+  //   event.preventDefault();
+  //   window.location.href = "transactionStatus.html";
+
+
+  // });
+
+  function redirectToTransactionStatus() {
+    window.location.href = "transactionStatus.html";
+  }
+  transferButton.addEventListener("click", redirectToTransactionStatus);
+
+  // function omar(){
+  //   alert("bahy");
+  // }
   // Event listener for "Show Details" button
   // showDetailsButton.addEventListener("click", function(event) {
   //   event.preventDefault();
@@ -82,4 +141,22 @@ document.addEventListener("DOMContentLoaded", function() {
     const showDetails = document.getElementById("ShowDetails");
     showDetails.style.display = "block";
   }
+
+
+  function validateDate() {
+    var selectedDate = new Date(document.getElementById("transactionDate").value);
+    var currentDate = new Date();
+  
+    if (selectedDate.getTime() !== currentDate.getTime()) {
+      alert("Please select today's date.");
+      // Clear the input field if an invalid date is chosen
+      document.getElementById("transactionDate").value = "";
+    }
+  }
+  
+  // Set the max attribute of the input element to today's date
+  var currentDate = new Date().toISOString().split("T")[0];
+  document.getElementById("transactionDate").setAttribute("max", currentDate);
+  
+  
 });
